cdd4926a4174875bb5cf882f31e9d9682b25894b
ayrton juarez <ayrtonjuarez90@gmail.com>
2022-04-20 13:14:05 -0300

    first commit

---
+++ rutas/index.ts
@@ -0,0 +1,27 @@
+import * as express from "express";
+const app = express();
+const port = 4000;
+
+app.get("/users/", function (req, res) {
+    res.json(["todos los usuarios"]);
+});
+
+app.post("/users/", function (req, res) {
+    res.json({
+        id: 1,
+        name: "usuarios creado",
+    });
+});
+
+app.patch("/users/:id", function (req, res) {
+    // los dos puntos en el path indica que lo que sigue es dinamico, esa magia la hace express
+    console.log(req.params);
+    res.json({
+        id: 1,
+        name: "usuarios modificado",
+    });
+});
+
+app.listen(port, () => {
+    console.log("El server esta funcioando en el puerto... " + port);
+});
