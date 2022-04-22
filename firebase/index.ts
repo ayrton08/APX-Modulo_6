1f2e008bda6a5c8f9d9dd99d5d8e15906866e71a
ayrton juarez <ayrtonjuarez90@gmail.com>
2022-04-22 01:09:07 -0300

    firestore

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,18 +1,35 @@
 import { firestore } from "./dataBase";
-import * as bodyparser from "body-parser";
+import * as bodyParser from "body-parser";
 import * as express from "express";
+const port = 3000;
 
 const app = express();
 
-app.use(bodyparser);
+app.use(bodyParser.json());
 
-const port = 4000;
+const usersCollection = firestore.collection("users")
+
 
 app.get("/users/", function (req, res) {
     res.json(["todos los usuarios"]);
 });
 
+app.get("/users/:userId", function (req, res) {
+    // los dos puntos en el get indica que lo que sigue es dinamico, esa magia la hace express
+    res.json({
+        message: "un usuario en particular",
+        params: req.params,
+    });
+});
+
 app.post("/users/", function (req, res) {
+    
+    usersCollection.doc().create(req.body).then(response => {
+        console.log(response)
+    }),
+
+    // usersCollection = doc() hace referencia a doc y create le graba la data que viene del req.body, en este caso la data que se esta mandando desde postman, en caso norrmal lo qeu hacen estas lineas es tomar la data que entra por el front y grabarla en la base de datos
+
     res.json({
         id: 1,
         name: "usuarios creado",
